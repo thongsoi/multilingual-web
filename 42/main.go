8e729957eb1ac4e246b8972a75847ac4a7812a13
Michena78 <phaow66@gmail.com>
2025-01-08 09:46:25 +0700

    update

--- 42/main.go
+++ 42/main.go
@@ -1,32 +1,31 @@
 package main
 
 import (
-	"encoding/json"
 	"html/template"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/gorilla/mux"
 )
 
 var (
-	templates = template.Must(template.ParseGlob("4/templates/*.html"))
+	templates = template.Must(template.ParseGlob("42/templates/*.html"))
 	locales   = make(map[string]map[string]string)
 )
 
 func main() {
-	// Load locale files
+	// Load locale data into memory
 	loadLocales()
 
 	// Create a new router
 	r := mux.NewRouter()
 
 	// Serve static files (if any)
-	r.PathPrefix("/4/static/").Handler(http.StripPrefix("/4/static/", http.FileServer(http.Dir("4/static"))))
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	// Routes
-	r.HandleFunc("/", homeHandler).Methods("GET")
+	r.HandleFunc("/", dynamicHandler("index")).Methods("GET")
+	r.HandleFunc("/about-us", dynamicHandler("about")).Methods("GET")
 	r.HandleFunc("/change-language", changeLanguageHandler).Methods("POST")
 
 	// Start the server
@@ -34,38 +33,54 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
-// Load locale files into memory
+// Load locale data into memory
 func loadLocales() {
-	languages := []string{"en", "es", "fr"}
-	for _, lang := range languages {
-		file, err := os.ReadFile("4/locales/" + lang + ".json")
-		if err != nil {
-			log.Fatalf("Error loading locale file for %s: %v", lang, err)
-		}
-		var data map[string]string
-		if err := json.Unmarshal(file, &data); err != nil {
-			log.Fatalf("Error parsing locale file for %s: %v", lang, err)
-		}
-		locales[lang] = data
+	// Define locale data directly in code
+	locales["en"] = map[string]string{
+		"title":       "Multilingual App",
+		"welcome":     "Welcome!",
+		"description": "This is a simple multilingual web application.",
+		"aboutTitle":  "About Us",
+		"aboutBody":   "We are a company dedicated to creating multilingual applications.",
+	}
+	locales["es"] = map[string]string{
+		"title":       "Aplicación Multilingüe",
+		"welcome":     "¡Bienvenido!",
+		"description": "Esta es una aplicación web multilingüe simple.",
+		"aboutTitle":  "Sobre Nosotros",
+		"aboutBody":   "Somos una empresa dedicada a crear aplicaciones multilingües.",
+	}
+	locales["fr"] = map[string]string{
+		"title":       "Application Multilingue",
+		"welcome":     "Bienvenue!",
+		"description": "Ceci est une application web multilingue simple.",
+		"aboutTitle":  "À Propos",
+		"aboutBody":   "Nous sommes une entreprise dédiée à la création d'applications multilingues.",
 	}
 }
 
-// Home handler
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	lang := r.URL.Query().Get("lang")
-	if lang == "" {
-		lang = "en" // Default language
-	}
+// Dynamic handler for different pages
+func dynamicHandler(page string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		lang := r.URL.Query().Get("lang")
+		if lang == "" {
+			lang = "en" // Default language
+		}
 
-	data := struct {
-		Lang    string
-		Content map[string]string
-	}{
-		Lang:    lang,
-		Content: locales[lang],
-	}
+		data := struct {
+			Lang    string
+			Page    string
+			Content map[string]string
+		}{
+			Lang:    lang,
+			Page:    page,
+			Content: locales[lang],
+		}
 
-	templates.ExecuteTemplate(w, "index.html", data)
+		if err := templates.ExecuteTemplate(w, page+".html", data); err != nil {
+			http.NotFound(w, r)
+		}
+	}
 }
 
 // Change language handler (HTMX endpoint)
